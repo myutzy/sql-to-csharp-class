420c6c2825b781db497509d6c1dc963d06994ed7
Matt Yutzy <1199572+myutzy@users.noreply.github.com>
2023-04-15 15:41:21 -0400

    MVP (#1)
    * Initial commit.
    
    * Organize into folders
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sql-to-csharp-class
-Python script to create a C# class from a SQL CREATE command.
+# SQL To C# Class
+Need a quick way to generate a C# class from a SQL `CREATE` command? Use this simple Python script. It's much faster than typing the class by hand.
+
+![image](https://user-images.githubusercontent.com/1199572/232250143-d1e6a49d-7273-40f7-9a18-ed9b399372cc.png)
+
+## Features
+- Interprets SQL column name, type, nullability and max length (if string) from input text
+- Customizable output property template
+
+## How do I use it?
+- Modify `Input.txt` to contain only the column portion of your SQL `CREATE` command. Refer to the initial contents of `Input.txt` for an example.
+- Run `python app.py`
+- Results will be saved as `Output.cs`
---
+++ src/app.py
@@ -0,0 +1,83 @@
+import re
+
+class_name = "MyClass"
+
+def decapitalize_first_letter(s, upper_rest = False):
+  return ''.join([s[:1].lower(), (s[1:].upper() if upper_rest else s[1:])]) 
+
+def parse_sql_create_command(sql_create_command):
+    column_regex = r'\[(.*?)\] \[(.*?)\](?:\((\d+)\))? (NULL|NOT NULL)'
+    matches = re.findall(column_regex, sql_create_command)
+    
+    columns = []
+    for match in matches:
+        column_name, sql_type, max_length, is_nullable = match
+        is_nullable = is_nullable == 'NULL'
+        max_length = int(max_length) if max_length else None
+        columns.append((column_name, sql_type, is_nullable, max_length))
+        
+    return columns
+
+def sql_type_to_csharp_type(sql_type, is_nullable):
+    # TODO: Implement additional SQL data types
+    if sql_type in ["int", "smallint"]:
+        return "int" + ("?" if is_nullable else "")
+    elif sql_type in ["nvarchar", "varchar"]:
+        return "string"
+    elif sql_type == "float":
+        return "double" + ("?" if is_nullable else "")
+    elif sql_type == "bit":
+        return "bool" + ("?" if is_nullable else "")
+    else:
+        raise ValueError(f"Unsupported SQL type: {sql_type}")
+
+def create_csharp_class(class_name, columns):
+    # Optionally apply any property template customizations here.
+    property_template = '''private {csharp_type} _{private_column_name};
+
+    {attributes}public {csharp_type} {column_name}
+    {{
+        get => _{private_column_name};
+        set => _{private_column_name} = value;
+    }}
+    '''
+
+    properties = []
+
+    for column_name, sql_type, is_nullable, max_length in columns:
+        csharp_type = sql_type_to_csharp_type(sql_type, is_nullable)
+        private_column_name = decapitalize_first_letter(column_name)
+        attributes = str()
+        if max_length:
+            attributes += f"[MaxLength({max_length})]\n    "
+        property_line = property_template.format(csharp_type=csharp_type, column_name=column_name, private_column_name=private_column_name, attributes=attributes)
+        properties.append(property_line)
+
+    class_template = "public class {class_name}\n{{\n{properties}\n}}"
+
+    properties_str = "\n".join("    " + prop for prop in properties)
+
+    return class_template.format(class_name=class_name, properties=properties_str)
+
+"""
+Expected input format is the column portion of the SQL create script only, not the entire script.
+Example Input.txt contents:
+
+[CustomerId] [int] NOT NULL,
+[FirstName] [nvarchar](50) NULL,
+[LastName] [nvarchar] NULL,
+[CompanyName] [nvarchar] NULL,
+[PhoneNumber] [nvarchar] NULL,
+[EmailAddress] [nvarchar] NULL
+"""
+
+with open('..\input\input.txt', 'r') as file:
+    column_definitions = file.read()
+
+columns = parse_sql_create_command(column_definitions)
+csharp_class = create_csharp_class(class_name, sorted(columns))
+
+with open('..\output\output.cs', 'w') as f:
+    f.write(csharp_class)
+
+print('Result saved to file.')
